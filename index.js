0772e578cf1c3644307f20e7f4c9cb139729c1ce
matvizzari <mat.vizzari@gmail.com>
2024-03-05 09:28:47 -0300

    prueba ratio

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ app.get('/', (req, res) => {
                 <meta property="og:image" content="https://www.cleverpdf.com/203786/images.gif" />
                 <meta property="fc:frame" content="vNext" />
                 <meta property="fc:frame:image" content="https://www.cleverpdf.com/203786/images.gif" />
+                <meta property="fc:frame:image:aspect_ratio=16:9" />
                 <meta property="fc:frame:button:1" content="Web 🍖" />
                 <meta property="fc:frame:button:1:action" content="link" />
                 <meta property="fc:frame:button:1:target" content="https://based.thelp.xyz/" />
