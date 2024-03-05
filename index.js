7aebb3925d702631ee8258895784835978df2145
matvizzari <mat.vizzari@gmail.com>
2024-03-05 10:00:23 -0300

    cambio valores ratio

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
                 <title>Frame Test</title>
                 <meta property="og:image" content="https://s9.gifyu.com/images/SUzsN.gif" />
                 <meta property="fc:frame" content="vNext" />
-                <meta property="fc:frame:image" content="https://s9.gifyu.com/images/SUzsN.gif" aspect_ratio="0.8"/>
+                <meta property="fc:frame:image" content="https://s9.gifyu.com/images/SUzsN.gif" aspect_ratio="2:1"/>
                 <meta property="fc:frame:button:1" content="Web 🍖" />
                 <meta property="fc:frame:button:1:action" content="link" />
                 <meta property="fc:frame:button:1:target" content="https://based.thelp.xyz/" />
